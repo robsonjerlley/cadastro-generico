b0d79553a3be485dfe82149a8ab8fc7213df47a8
robsonjerlley <robsonjerlley.work@gmail.com>
2025-10-27 16:49:48 -0300

    Frist commit

---
+++ src/main/java/com/robsonjerlley/dev/cadastro_generico/CadastroGenericoApplication.java
@@ -0,0 +1,13 @@
+package com.robsonjerlley.dev.cadastro_generico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroGenericoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroGenericoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robsonjerlley/dev/cadastro_generico/CadastroGenericoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robsonjerlley.dev.cadastro_generico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroGenericoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

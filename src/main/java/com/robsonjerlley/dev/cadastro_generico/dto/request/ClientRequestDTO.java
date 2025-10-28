b42cbe4dbd1dd14e6b51ff901b6da5289518f1d1
robsonjerlley <robsonjerlley.work@gmail.com>
2025-10-28 08:21:37 -0300

    Iniciando as definições dos DTOs.

--- src/main/java/com/robsonjerlley/dev/cadastro_generico/dto/request/ClientRequestDTO.java
+++ src/main/java/com/robsonjerlley/dev/cadastro_generico/dto/request/ClientRequestDTO.java
@@ -1,4 +1,44 @@
 package com.robsonjerlley.dev.cadastro_generico.dto.request;
 
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.OffsetDateTime;
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class ClientRequestDTO {
+
+    @NotBlank(message = "{NotBlank}")
+    @Size(min = 3, max = 100, message = "{Size}")
+    private String name;
+    @NotBlank(message = "{NotBlank}")
+    @CPF
+    private String cpf;
+
+    @NotBlank(message = "{NotBlank}")
+    @Size(min =7, max= 9, message = "O campo deve ser preenchido apenas com os digitos" )
+    private String rg;
+    @NotNull(message = "{NotNull}")
+    @Past(message = "Data inválida")
+    private OffsetDateTime dateBirth;
+
+    public ClientRequestDTO() {
+    }
+
+    public ClientRequestDTO(String name, String cpf, String rg, OffsetDateTime dateBirth) {
+        this.name = name;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.dateBirth = dateBirth;
+    }
 }
--- src/main/java/com/robsonjerlley/dev/cadastro_generico/dto/response/ClientResponseDTO.java
+++ src/main/java/com/robsonjerlley/dev/cadastro_generico/dto/response/ClientResponseDTO.java
@@ -1,4 +1,18 @@
 package com.robsonjerlley.dev.cadastro_generico.dto.response;
 
+import com.robsonjerlley.dev.cadastro_generico.model.Address;
+import com.robsonjerlley.dev.cadastro_generico.model.Contact;
+
+import java.time.OffsetDateTime;
+
 public class ClientResponseDTO {
+    private String name;
+    private String cpf;
+    private OffsetDateTime dateBirth;
+    private AddressResposeDTO addressResposeDTO;
+    private ContactResposeDTO contactResposeDTO;
+
+
+    public ClientResponseDTO() {
+    }
 }

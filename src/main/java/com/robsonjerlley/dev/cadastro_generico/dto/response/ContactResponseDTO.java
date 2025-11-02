f37cada4577a72f9c2ff34f7fa791de139859b14
robsonjerlley <robsonjerlley.work@gmail.com>
2025-11-02 16:13:55 -0300

    Corigindo filds em ContactMapper e ContactResponseDTO.

--- src/main/java/com/robsonjerlley/dev/cadastro_generico/dto/response/ContactResponseDTO.java
+++ src/main/java/com/robsonjerlley/dev/cadastro_generico/dto/response/ContactResponseDTO.java
@@ -1,4 +1,26 @@
 package com.robsonjerlley.dev.cadastro_generico.dto.response;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class ContactResponseDTO {
+    private String tell;
+    private String secondTell;
+    private String email;
+
+
+    public ContactResponseDTO() {
+    }
+
+    public ContactResponseDTO(String tell, String secondTell, String email) {
+        this.tell = tell;
+        this.secondTell = secondTell;
+        this.email = email;
+    }
 }
--- src/main/java/com/robsonjerlley/dev/cadastro_generico/mapper/ContactMapper.java
+++ src/main/java/com/robsonjerlley/dev/cadastro_generico/mapper/ContactMapper.java
@@ -1,10 +1,12 @@
 package com.robsonjerlley.dev.cadastro_generico.mapper;
 
+import com.robsonjerlley.dev.cadastro_generico.dto.request.ContactRequestDTO;
 import com.robsonjerlley.dev.cadastro_generico.dto.response.ContactResponseDTO;
 import com.robsonjerlley.dev.cadastro_generico.model.Contact;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface ContactMapper {
+    Contact toEntity(ContactRequestDTO requestDTO);
     ContactResponseDTO toResponseDTO(Contact entity);
 }
